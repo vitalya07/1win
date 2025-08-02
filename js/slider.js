414e9979b150ec8ef58f7b5fb2aa51a3cd5cfe5f
Vitalyi <saifutdinov_vitalii@mail.ru>
2025-08-02 15:55:59 +0500

    add to section games

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,49 @@
-new Swiper(".main__swiper", {
+const mainSwiper = new Swiper(".main__swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+const winGamesSwiper = new Swiper(".win-games__swiper", {
+    slidesPerView: 6,
+    spaceBetween: 17,
+    // slidesPerGroup: 3,
+    loop: true,
+    //  centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+const allGamesSwiper = new Swiper(".all-games__swiper", {
+    slidesPerView: 6,
+    spaceBetween: 17,
+    // slidesPerGroup: 3,
+    loop: true,
+    //  centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+const liveCasinoSwiper = new Swiper(".live-casino-swiper", {
+    slidesPerView: 6,
+    spaceBetween: 17,
+    // slidesPerGroup: 3,
+    loop: true,
+    //  centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const popularSwiper = new Swiper(".popular-swiper", {
+    slidesPerView: 6,
+    spaceBetween: 17,
+    // slidesPerGroup: 3,
+    loop: true,
+    //  centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
