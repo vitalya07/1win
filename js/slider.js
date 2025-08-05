6c9093b676343628f89e6c61d333fbff3ff85b40
Vitalii <saifutdinov_vitalii@mail.ru>
2025-08-05 15:53:15 +0500

    fdfsf

--- js/slider.js
+++ js/slider.js
@@ -7,45 +7,98 @@ const mainSwiper = new Swiper(".main__swiper", {
 const winGamesSwiper = new Swiper(".win-games__swiper", {
     slidesPerView: 6,
     spaceBetween: 17,
-    // slidesPerGroup: 3,
     loop: true,
-    //  centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    // breakpoints: {
+    //     640: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+    //     },
+    //     // когда ширина экрана >= 768px
+    //     768: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //     },
+    //     // когда ширина экрана >= 1024px
+    //     1024: {
+    //     slidesPerView: 4,
+    //     spaceBetween: 40,
+    //     },
+    // },
 });
 const allGamesSwiper = new Swiper(".all-games__swiper", {
-    slidesPerView: 6,
+    slidesPerView: 4,
     spaceBetween: 17,
-    // slidesPerGroup: 3,
     loop: true,
-    //  centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    // breakpoints: {
+    //     640: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+    //     },
+    //     // когда ширина экрана >= 768px
+    //     768: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //     },
+    //     // когда ширина экрана >= 1024px
+    //     1024: {
+    //     slidesPerView: 4,
+    //     spaceBetween: 40,
+    //     },
+    // },
 });
 const liveCasinoSwiper = new Swiper(".live-casino-swiper", {
     slidesPerView: 6,
-    spaceBetween: 17,
-    // slidesPerGroup: 3,
     loop: true,
-    //  centeredSlides: true,
+    spaceBetween: 10,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    // breakpoints: {
+    //     640: {
+    //     slidesPerView: 2,
+    //     },
+    //     // когда ширина экрана >= 768px
+    //     768: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //     },
+    //     // когда ширина экрана >= 1024px
+    //     1024: {
+    //     slidesPerView: 4,
+    //     spaceBetween: 40,
+    //     },
+    // },
 });
 
 const popularSwiper = new Swiper(".popular-swiper", {
-    slidesPerView: 6,
-    spaceBetween: 17,
-    // slidesPerGroup: 3,
+    slidesPerView: 1,
+    spaceBetween: 10,
     loop: true,
-    //  centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
     },
 });
